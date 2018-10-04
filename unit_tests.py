8e05c228dcd8c3c17561ae4aa3bca0ccb19c8201
jclane <justin@justinlane.me>
2018-10-04 18:36:04 -0400

    Cleaned up unit tests.

--- unit_tests.py
+++ unit_tests.py
@@ -1,11 +1,14 @@
 from game_of_life import next_board_state
 from game_of_life import render
 
-# TODO: there's a lot of repeated code here. Can
-# you move some of into reusable functions to
-# make it shorter and neater?
+"""
+ TODO: there's a lot of repeated code here. Can
+ you move some of into reusable functions to
+ make it shorter and neater?
+"""
 
-def test(test_num, init_state, expected_next_state, actual_next_state):
+
+def test(test_num, expected_next_state, actual_next_state):
     if expected_next_state == actual_next_state:
         print("PASSED", test_num)
     else:
@@ -14,6 +17,7 @@ def test(test_num, init_state, expected_next_state, actual_next_state):
         render(expected_next_state)
         print("Actual:")
         render(actual_next_state)
+
 
 if __name__ == "__main__":
     # TEST 1: dead cells with no live neighbors
@@ -31,7 +35,7 @@ if __name__ == "__main__":
     ]
     actual_next_state1 = next_board_state(init_state1)
     
-    test("1", init_state1, expected_next_state1, actual_next_state1)
+    test("1", expected_next_state1, actual_next_state1)
 
     # TEST 2: dead cells with exactly 3 neighbors
     # should come alive.
@@ -47,7 +51,7 @@ if __name__ == "__main__":
     ]
     actual_next_state2 = next_board_state(init_state2)
 
-    test("2", init_state2, expected_next_state2, actual_next_state2)
+    test("2", expected_next_state2, actual_next_state2)
     
     # TEST 3: live cells wih more thn 3 live neighbors
     # should die
@@ -64,7 +68,7 @@ if __name__ == "__main__":
     ]
     actual_next_state3 = next_board_state(init_state3)
 
-    test("3", init_state3, expected_next_state3, actual_next_state3)
+    test("3", expected_next_state3, actual_next_state3)
     
     # TEST 4: cells on the edges of the board behave 
     # as expected
@@ -81,7 +85,7 @@ if __name__ == "__main__":
     ]
     actual_next_state4 = next_board_state(init_state4)
 
-    test("4", init_state4, expected_next_state4, actual_next_state4)
+    test("4", expected_next_state4, actual_next_state4)
     
     # TEST 5: dead cells in corners should come alive
     # if neighbored by 3 live cells
@@ -98,4 +102,4 @@ if __name__ == "__main__":
     ]
     actual_next_state5 = next_board_state(init_state5)
 
-    test("5", init_state5, expected_next_state5, actual_next_state5)
+    test("5", expected_next_state5, actual_next_state5)
